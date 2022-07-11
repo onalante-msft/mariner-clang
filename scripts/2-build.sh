88f637a84fd1677f86d7e6d4292556dd6d46ba2b
onalante-msft <89409054+onalante-msft@users.noreply.github.com>
2022-07-10 23:13:35 -0700

    Fetch source before build

--- scripts/2-build.sh
+++ scripts/2-build.sh
@@ -1,5 +1,12 @@
 #! /bin/sh -eux
 
+rm -rf SPECS/clang/SOURCES
+mkdir SPECS/clang/SOURCES
+(
+    cd SPECS/clang/SOURCES;
+    curl -LO "https://github.com/llvm/llvm-project/releases/download/llvmorg-8.0.1/cfe-8.0.1.src.tar.xz";
+)
+
 rm -rf SPECS/mariner-clang/SOURCES
 mkdir SPECS/mariner-clang/SOURCES
 cp main.c SPECS/mariner-clang/SOURCES
@@ -7,7 +14,7 @@ cp main.c SPECS/mariner-clang/SOURCES
 tar -xvzf toolkit.tar.gz
 cd toolkit
 make \
-  PACKAGE_BUILD_LIST="mariner-clang" \
+  PACKAGE_BUILD_LIST="clang mariner-clang" \
   USE_PREVIEW_REPO=n \
   CONFIG_FILE= \
   -j "$(nproc)" \
